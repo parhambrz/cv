f496979ac91eee69cee76a015e20e76b23eb7ec1
parhambarazesh <parham.barazesh@bouvet.no>
2026-03-23 21:24:08 +0100

    Add fontsource dependencies and update CSS imports for Source Sans 3 and Space Grotesk

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
+import '@fontsource/source-sans-3/latin-400.css'
+import '@fontsource/source-sans-3/latin-600.css'
+import '@fontsource/source-sans-3/latin-700.css'
+import '@fontsource/space-grotesk/latin-500.css'
+import '@fontsource/space-grotesk/latin-700.css'
 import './index.css'
 import App from './App.jsx'
 
